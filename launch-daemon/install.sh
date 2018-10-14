ecb304fd68aa015eeaf9862fbba8830e01143c3e
Naookie Sato <necrosato@live.com>
2018-10-14 16:07:55 -0700

    dirname

--- launch-daemon/install.sh
+++ launch-daemon/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+CDIR=$(pwd)
+cd $(dirname $0)
+
 cp iron_biscotti.sh /var/root/.iron_biscotti.sh
 chown root:wheel /var/root/.iron_biscotti.sh
 cp com.iron_biscotti.plist /Library/LaunchDaemons/com.iron_biscotti.plist
 chown root:wheel /Library/LaunchDaemons/com.iron_biscotti.plist
 
 launchctl load -w /Library/LaunchDaemons/com.iron_biscotti.plist
+cd $CDIR
