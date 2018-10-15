ec7724519781d0c02a52871dd6a4d10bf8f4f7b0
mn <39255541+margielamadman@users.noreply.github.com>
2018-10-14 21:01:32 -0700

    Add find_port function

--- launch-daemon/iron_biscotti.sh
+++ launch-daemon/iron_biscotti.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# TODO: This should not be a static value. Should ensure that the port is available.
+IP=127.0.0.1
+PORT_BEGIN=10000
 SSHD_PORT=10000
 SSHD_COMMAND="/usr/sbin/sshd -p $SSHD_PORT"
 SSHD_STATUS=0
+
+function find_port() {
+  for ((port=$PORT_BEGIN; port<=$PORT_BEGIN+1000; port++))
+    do
+      nc -z $IP $port
+      if [ "$?" -eq 1 ]; then
+        SSHD_PORT=$port	
+	break
+      fi
+    done
+}
 
 function check_sshd() {
   SSHD_PID=$(ps -ax | grep "$SSHD_COMMAND" | grep -v "grep" | awk '{print $1}')
@@ -27,6 +39,7 @@ function check_ssh() {
 }
 
 while true; do
+  find_port
   check_sshd
   if [ "$SSHD_STATUS" == "0" ]; then
     check_ssh
