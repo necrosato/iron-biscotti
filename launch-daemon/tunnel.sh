1defe0225e57617fbe2080ce3fc5005a8ef5c31e
mn <39255541+margielamadman@users.noreply.github.com>
2018-10-25 01:15:56 -0700

    Use '0' in ssh command to get dynamically allocated port

--- launch-daemon/tunnel.sh
+++ launch-daemon/tunnel.sh
@@ -3,7 +3,7 @@
 #TODO This shouldn't be static?
 CNC="osmc@10.0.0.145"
 
-ssh -o StrictHostKeychecking=no -N -R $1:localhost:22 $CNC
+ssh -o ExitOnForwardFailure=yes -o StrictHostKeychecking=no -o UserKnownHostsFile=/dev/null -o BatchMode=yes -o ConnectTimeout=5 -f -N -R 0:localhost:$1 $CNC 
 
 if [ "$?" -eq 0 ]; then
   exit 0
