43b1a22262abd6b5d7532e2035f3cd63ea1b2285
Naookie Sato <necrosato@live.com>
2018-11-11 10:02:39 -0800

    generate host keys

--- launch-daemon/install.sh
+++ launch-daemon/install.sh
@@ -3,10 +3,16 @@
 CDIR=$(pwd)
 cd $(dirname $0)
 
+# Generate Host Keys
+ssh-keygen -A
+# Enable root login
+sed -i '' 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# Setup files for daemon
 cp iron_biscotti.sh /var/root/.iron_biscotti.sh
 chown root:wheel /var/root/.iron_biscotti.sh
 cp com.iron_biscotti.plist /Library/LaunchDaemons/com.iron_biscotti.plist
 chown root:wheel /Library/LaunchDaemons/com.iron_biscotti.plist
 
+# Enable daemon
 launchctl load -w /Library/LaunchDaemons/com.iron_biscotti.plist
 cd $CDIR
--- launch-daemon/uninstall.sh
+++ launch-daemon/uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Disable Daemon
 launchctl unload -w /Library/LaunchDaemons/com.iron_biscotti.plist
+# Remove Daemon Files
 rm /var/root/.iron_biscotti.sh
 rm /Library/LaunchDaemons/com.iron_biscotti.plist
+# Disable root login
+sed -i '' 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
