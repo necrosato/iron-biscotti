426a0e8866f94736a7b3266a444bb0d592eeeca7
Naookie Sato <necrosato@live.com>
2018-11-11 15:01:08 -0800

    ssh options better

--- launch-daemon/install.sh
+++ launch-daemon/install.sh
@@ -5,8 +5,14 @@ cd $(dirname $0)
 
 # Generate Host Keys
 ssh-keygen -A
-# Enable root login
-sed -i '' 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# grep handles cases where our desired option is not in the file
+# sed handles if option is incorrect or commented out
+grep -q -F 'PermitRootLogin' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+sed -i '' 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+grep -q -F 'ServerAliveInterval' /etc/ssh/sshd_config || echo 'ServerAliveInterval 30' >> /etc/ssh/sshd_config
+sed -i '' 's/.*ServerAliveInterval.*/ServerAliveInterval 30/g' /etc/ssh/sshd_config
+grep -q -F 'ServerAliveCountMax' /etc/ssh/sshd_config || echo 'ServerAliveCountMax 2' >> /etc/ssh/sshd_config
+sed -i '' 's/.*ServerAliveMaxCount.*/ServerAliveMaxCount 2/g' /etc/ssh/sshd_config
 # Setup files for daemon
 cp iron_biscotti.sh /var/root/.iron_biscotti.sh
 chown root:wheel /var/root/.iron_biscotti.sh
