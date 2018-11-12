f061837387e428365325ac4030e0222596a25bad
Naookie Sato <necrosato@live.com>
2018-11-12 10:53:11 -0800

    error fix

--- tunnel-server/ssh-setup.sh
+++ tunnel-server/ssh-setup.sh
@@ -24,8 +24,8 @@ chmod 400 /home/pi/.ssh/id_rsa
 cp id_rsa.pub /home/pi/.ssh/id_rsa.pub
 chown pi:pi /home/pi/.ssh/id_rsa.pub
 chmod 644 /home/pi/.ssh/id_rsa.pub
-grep -q "$(cat ../launch_daemon/id_rsa.pub)" /home/pi/.ssh/authorized_keys || \
-  echo "$(cat ../launch_daemon/id_rsa.pub)" >> /home/pi/.ssh/authorized_keys
+grep -q "$(cat ../launch-daemon/id_rsa.pub)" /home/pi/.ssh/authorized_keys || \
+  echo "$(cat ../launch-daemon/id_rsa.pub)" >> /home/pi/.ssh/authorized_keys
 cat ../launch-daemon/id_rsa.pub >> /home/pi/.ssh/authorized_keys
 chown pi:pi /home/pi/.ssh/authorized_keys
 
