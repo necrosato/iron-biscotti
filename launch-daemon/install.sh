4456affe8cf41705872fc177779ab4b6d1d40268
Naookie Sato <necrosato@live.com>
2018-11-11 22:05:15 -0800

    ready for review i think

--- launch-daemon/install.sh
+++ launch-daemon/install.sh
@@ -10,9 +10,9 @@ sed -i '' 's/#* *PermitRootLogin .*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i '' 's/#* *ServerAliveInterval .*/ServerAliveInterval 30/g' /etc/ssh/ssh_config
 sed -i '' 's/#* *ServerAliveCountMax .*/ServerAliveCountMax 2/g' /etc/ssh/ssh_config
 # grep handles cases where our desired option is not in the file
-grep -q -F '^PermitRootLogin ' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-grep -q -F '^ServerAliveInterval ' /etc/ssh/ssh_config || echo 'ServerAliveInterval 30' >> /etc/ssh/ssh_config
-grep -q -F '^ServerAliveCountMax ' /etc/ssh/ssh_config || echo 'ServerAliveCountMax 2' >> /etc/ssh/ssh_config
+grep -q '^PermitRootLogin ' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+grep -q '^ServerAliveInterval ' /etc/ssh/ssh_config || echo 'ServerAliveInterval 30' >> /etc/ssh/ssh_config
+grep -q '^ServerAliveCountMax ' /etc/ssh/ssh_config || echo 'ServerAliveCountMax 2' >> /etc/ssh/ssh_config
 # Setup files for daemon
 cp iron_biscotti.sh /var/root/.iron_biscotti.sh
 chown root:wheel /var/root/.iron_biscotti.sh
